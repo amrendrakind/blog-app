b01f978594518d27e02fc396d82deed3bacdd3d0
amrendrakind <vision2010@gmail.com>
2022-07-14 03:21:38 +0530

    Update: Relations and Migration done

--- db/migrate/20220713214100_create_posts.rb
+++ db/migrate/20220713214100_create_posts.rb
@@ -1,7 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.integer :author_id
       t.text :title
       t.text :text
       t.integer :likes_counter
--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_191447) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_214817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "post_id"
     t.text "text"
     t.string "timestamps"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "post_id"
     t.string "timestamps"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
     t.text "title"
     t.text "text"
     t.integer "likes_counter"
     t.string "timestamps"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +56,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_191447) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
