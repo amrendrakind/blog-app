1eec8ffbfa1138dea5d240a48d9a36d8dc1536c9
amrendrakind <vision2010@gmail.com>
2022-07-16 00:41:13 +0530

    Update: update of likes counter test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -10,13 +10,22 @@ RSpec.describe Like, type: :model do
                         likes_counter: 0)
     @post.save
 
-    @like = Like.create(post: @post, author: @first_user)
+    @like = Like.create(post_id: @post.id, author_id: @first_user.id)
     @like.save
   end
   context 'Likes validations' do
     it 'validate update_likes_counter method' do
       @like.update_likes_counter
       expect(@like.post.likes_counter).to be > 0
+    end
+    it 'Must belog to a user' do
+      @like.author_id = nil
+      expect(@like).to_not be_valid
+    end
+
+    it 'Must belog to a post' do
+      @like.post_id = nil
+      expect(@like).to_not be_valid
     end
   end
 end
