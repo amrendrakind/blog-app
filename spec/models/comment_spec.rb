9f314150daa390d3a2da8f99c86f51310723050f
amrendrakind <vision2010@gmail.com>
2022-07-16 04:32:49 +0530

    Linter error fixed

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   before :each do
     @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                              bio: 'Teacher from Mexico.', post_counter: 0)
+                        bio: 'Teacher from Mexico.', post_counter: 0)
     @user.save
     @post = Post.create(author_id: @user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0,
                         likes_counter: 0)
