ae56ed154fd8f99f3588c11c720e50215641f1d5
amrendrakind <vision2010@gmail.com>
2022-07-15 23:33:55 +0530

    Linter error fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,13 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  
+
   # validations for title, comments_counter and likes_counter
-  
+
   validates :title, presence: true
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
+
   # Method that updates the posts counter for a user.
 
   def update_posts_counter
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  #validations for name and post_counter
+  # validations for name and post_counter
 
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
