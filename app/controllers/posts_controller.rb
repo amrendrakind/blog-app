6581a125c0c458b837a86cc9f1cda01803b6bc98
amrendrakind <vision2010@gmail.com>
2022-07-21 02:05:09 +0530

    Update: View for User and Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,9 @@
 class PostsController < ApplicationController
   def index
-    # user_info = User.find(params[:user_id])
-    # @posts = user_info.posts.all
-    @user=User.includes(:posts, :comments).find(params[:id])
+    @user=User.includes(:posts, :comments).find(params[:user_id])
   end
 
   def show
-    # user_info = User.find(params[:user_id])
-    # puts params
-    # @post = user_info.posts.find(params[:id])
     user=User.find(params[:id])
   end
 end
