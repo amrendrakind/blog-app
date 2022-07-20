81166567f353f74fe973eb79c35c312258f6ae60
amrendrakind <vision2010@gmail.com>
2022-07-20 20:57:43 +0530

    Update: Views of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    user_info = User.find(params[:user_id])
+    @posts = user_info.posts.all
+  end
 
-  def show; end
+  def show
+    user_info = User.find(params[:user_id])
+    puts params
+    @post = user_info.posts.find(params[:id])
+  end
 end
