d268c1262b89468618e0c6629ae7cc17f84bccca
amrendrakind <vision2010@gmail.com>
2022-07-22 01:05:20 +0530

    Update: Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
 class PostsController < ApplicationController
   def index
     @user=User.includes(:posts, :comments).find(params[:user_id])
+    @posts = User.find(params[:id]).recent_posts
   end
 
   def show
     author_id = params["user_id"]
     post_id = params["id"]
-
+    @user = User.find(author_id)
     @post=User.includes(:posts, :comments).find(author_id).posts.find( post_id)
-
+    @comments = @post.recent_comments
   end
 end
