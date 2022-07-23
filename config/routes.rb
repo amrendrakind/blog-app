139d73d081170f6f513f18f0b34a93e4ff787f24
amrendrakind <vision2010@gmail.com>
2022-07-23 10:01:33 +0530

    Add: Like button

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "users#index"
+  root "users#index"
   # resources :logins
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
-      resources :likes, only: [:create]
+      resources :likes, only: [:new, :create]
     end
   end
   resources :login, only: [:index, :show] do
