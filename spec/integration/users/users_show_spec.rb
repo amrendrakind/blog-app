9eb64ccc29a0a62ece20c0e8196f0eb41d808708
amrendrakind <vision2010@gmail.com>
2022-07-26 23:59:09 +0530

    Integration testing: User Show

--- spec/integration/users/users_show_spec.rb
+++ spec/integration/users/users_show_spec.rb
@@ -5,5 +5,17 @@ RSpec.describe 'User index page', type: :system do
     User.destroy_all
     @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                               bio: 'Teacher from Mexico.', post_counter: 0)
+    @second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
+                              bio: 'Teacher from Poland.', post_counter: 0)
   end
+
+  it 'I can see the users profile picture.' do
+    visit "/users/#{@first_user.id}"
+    expect(page).to have_css("img[src*='https://unsplash.com/photos/F_-0BxGuVvo']")
+    visit "/users/#{@second_user.id}"
+    expect(page).to have_css("img[src*='https://unsplash.com/photos/F_-0BxGuVvo']")
+  end
+
+
+
 end
