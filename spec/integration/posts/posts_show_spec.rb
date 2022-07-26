5dfa21559807f3aba5a1ee041b9c6f76c4edeb60
amrendrakind <vision2010@gmail.com>
2022-07-27 05:01:33 +0530

    Linter error fixed

--- spec/integration/posts/posts_show_spec.rb
+++ spec/integration/posts/posts_show_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'User post show page', type: :system do
   before(:all) do
-    @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',bio: 'Teacher from Mexico.', post_counter: 0)
+    @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                              bio: 'Teacher from Mexico.', post_counter: 0)
 
-    @first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post',comments_counter: 0, likes_counter: 0)
-    @first_comment = Comment.create(post_id: @first_post.id, author_id: @first_user.id,text: 'This is my first comment')
+    @first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post',
+                              comments_counter: 0, likes_counter: 0)
+    @first_comment = Comment.create(post_id: @first_post.id, author_id: @first_user.id,
+                                    text: 'This is my first comment')
     @first_comment.update_comments_counter
-    @first_like= Like.create(post_id: @first_post.id, author_id: @first_user.id)
+    @first_like = Like.create(post_id: @first_post.id, author_id: @first_user.id)
     @first_like.update_likes_counter
   end
 
@@ -16,27 +19,27 @@ RSpec.describe 'User post show page', type: :system do
     expect(page).to have_content(@first_post.title)
   end
 
-  it "I can see who wrote the post." do
+  it 'I can see who wrote the post.' do
     visit "/users/#{@first_user.id}/posts/#{@first_post.id}"
     expect(page).to have_content(@first_user.name)
   end
 
-  it "I can see how many comments it has." do
+  it 'I can see how many comments it has.' do
     visit "/users/#{@first_user.id}/posts/#{@first_post.id}"
-    expect(page).to have_content ('Comments:1')
+    expect(page).to have_content('Comments:1')
   end
 
-  it "I can see how many likes it has." do
+  it 'I can see how many likes it has.' do
     visit "/users/#{@first_user.id}/posts/#{@first_post.id}"
-    expect(page).to have_content ('Likes:1')
+    expect(page).to have_content('Likes:1')
   end
-  
-  it "I can see the post body." do
+
+  it 'I can see the post body.' do
     visit "/users/#{@first_user.id}/posts/#{@first_post.id}"
     expect(page).to have_content(@first_post.text)
   end
 
-  it "I can see the username of each commentor." do
+  it 'I can see the username of each commentor.' do
     visit "/users/#{@first_user.id}/posts/#{@first_post.id}"
     expect(page).to have_content("Username: #{@first_user.name}")
   end
@@ -45,5 +48,4 @@ RSpec.describe 'User post show page', type: :system do
     visit "/users/#{@first_user.id}/posts/#{@first_post.id}"
     expect(page).to have_content(@first_comment.text)
   end
-
 end
