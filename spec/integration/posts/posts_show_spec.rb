27e97a55e053dfd77b77390f7ca49739cca983ad
amrendrakind <vision2010@gmail.com>
2022-07-27 03:19:25 +0530

    Integration testing: Posts show - post's title

--- spec/integration/posts/posts_show_spec.rb
+++ spec/integration/posts/posts_show_spec.rb
@@ -1,15 +1,16 @@
+require 'rails_helper'
+
 RSpec.describe 'User post index page', type: :system do
   before(:all) do
-    @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                              bio: 'Teacher from Mexico.', post_counter: 0)
+    @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',bio: 'Teacher from Mexico.', post_counter: 0)
 
-    @first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post',
-                              comments_counter: 0, likes_counter: 0)
-    @second_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my second post',
-                               comments_counter: 0, likes_counter: 0)
-    @third_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my third post',
-                              comments_counter: 0, likes_counter: 0)
-    @fourth_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my fourth post',
-                               comments_counter: 0, likes_counter: 0)
+    @first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post',comments_counter: 0, likes_counter: 0)
+    @first_comment = Comment.create(post_id: @first_post.id, author_id: @first_user.id,text: 'This is my first comment')
   end
+
+  it "I can see the post's title." do
+    visit "/users/#{@first_user.id}/posts/#{@first_post.id}"
+    expect(page).to have_content(@first_post.title)
+  end
+
 end
