0e3d326f33bf192553e9017993dcc2230c22adb0
amrendrakind <vision2010@gmail.com>
2022-07-27 00:19:36 +0530

    Linter error fixed

--- spec/integration/users/users_show_spec.rb
+++ spec/integration/users/users_show_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe 'User index page', type: :system do
     User.destroy_all
     @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                               bio: 'Teacher from Mexico.', post_counter: 0)
-    @second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
-                              bio: 'Teacher from Poland.', post_counter: 0)
+    @second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                               bio: 'Teacher from Poland.', post_counter: 0)
   end
 
   it 'I can see the users profile picture.' do
@@ -17,7 +17,7 @@ RSpec.describe 'User index page', type: :system do
   end
 
   it 'I can see the username of all other users' do
-    visit "/users/#{ @first_user.id}"
+    visit "/users/#{@first_user.id}"
     expect(page).to have_content('Tom')
     visit "/users/#{@second_user.id}"
     expect(page).to have_content('Lilly')
@@ -27,14 +27,14 @@ RSpec.describe 'User index page', type: :system do
     first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post',
                              comments_counter: 0, likes_counter: 0)
     first_post.update_posts_counter
-    visit "/users/#{ @first_user.id}"
+    visit "/users/#{@first_user.id}"
     expect(page).to have_content('Number of posts: 1')
-    visit "/users/#{ @second_user.id}"
+    visit "/users/#{@second_user.id}"
     expect(page).to have_content('Number of posts: 0')
   end
 
   it "I can see the user's bio." do
-    visit "/users/#{ @first_user.id}"
+    visit "/users/#{@first_user.id}"
     expect(page).to have_content('Bio')
     expect(page).to have_content('Teacher from Mexico.')
     visit "/users/#{@second_user.id}"
@@ -43,38 +43,47 @@ RSpec.describe 'User index page', type: :system do
   end
 
   it "I can see the user's first 3 posts." do
+    @first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post',
+                              comments_counter: 0, likes_counter: 0)
+    @second_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my second post',
+                               comments_counter: 0, likes_counter: 0)
+    @third_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my third post',
+                              comments_counter: 0, likes_counter: 0)
+    @fourth_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my fourth post',
+                               comments_counter: 0, likes_counter: 0)
 
-    first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
-    second_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
-    third_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
-    fourth_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my fourth post', comments_counter: 0, likes_counter: 0)     
-    
-    visit "/users/#{ @first_user.id}"
-    expect(page).to have_content('Post #', maximum:3) 
-    visit "/users/#{ @second_user.id}"
-    expect(page).to have_content('Post #', maximum:3) 
+    visit "/users/#{@first_user.id}"
+    expect(page).to have_content('Post #', maximum: 3)
+    visit "/users/#{@second_user.id}"
+    expect(page).to have_content('Post #', maximum: 3)
   end
 
   it "I can see a button that lets me view all of a user's posts." do
-    @first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
-    visit "/users/#{ @first_user.id}"
-   expect(page).to have_button('See all posts')
+    @first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post',
+                              comments_counter: 0, likes_counter: 0)
+    visit "/users/#{@first_user.id}"
+    expect(page).to have_button('See all posts')
   end
 
   it "When I click a user's post, it redirects me to that post's show page." do
-    @first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
-     visit "/users/#{ @first_user.id}"
+    @first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post',
+                              comments_counter: 0, likes_counter: 0)
+    visit "/users/#{@first_user.id}"
     click_link("Post ##{@first_post.id}")
-    expect(page).to have_content('This is my first post') 
-   end
+    expect(page).to have_content('This is my first post')
+  end
 
-    it "When I click to see all posts, it redirects me to the user's post's index page." do
-        first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
-        second_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
-        third_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
-        fourth_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my fourth post', comments_counter: 0, likes_counter: 0)     
-        visit "/users/#{ @first_user.id}"
-        click_button 'See all posts'
-        expect(page).to have_content('Post #', minimum:1) 
-    end
+  it "When I click to see all posts, it redirects me to the user's post's index page." do
+    @first_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post',
+                              comments_counter: 0, likes_counter: 0)
+    @second_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my second post',
+                               comments_counter: 0, likes_counter: 0)
+    @third_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my third post',
+                              comments_counter: 0, likes_counter: 0)
+    @fourth_post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my fourth post',
+                               comments_counter: 0, likes_counter: 0)
+    visit "/users/#{@first_user.id}"
+    click_button 'See all posts'
+    expect(page).to have_content('Post #', minimum: 1)
+  end
 end
