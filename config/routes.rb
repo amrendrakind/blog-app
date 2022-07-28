1c8571d7dc42f5bbf560efa5e390001c18652eb5
amrendrakind <vision2010@gmail.com>
2022-07-29 00:12:30 +0530

    Update: home link

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root to: "home#index"
-  devise_for :users
- 
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  
+  # get 'home/index'
+  root "home#index"
+  devise_for :users
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
