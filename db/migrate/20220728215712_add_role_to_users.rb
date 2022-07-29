0406420d566afb49e7c97ada20f8d20f7e088b18
amrendrakind <vision2010@gmail.com>
2022-07-29 19:39:55 +0530

    Update: role auth

--- db/migrate/20220728215712_add_role_to_users.rb
+++ db/migrate/20220728215712_add_role_to_users.rb
@@ -1,5 +1,4 @@
 class AddRoleToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :role, :string, default: 'user'
-  end
+    add_column :users, :role, :string
 end
--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_28_215712) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "role", default: "user"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
