449266b9cfdc43498886f08c0e49f98185e1a8fa
amrendrakind <vision2010@gmail.com>
2022-07-30 04:15:44 +0530

    Update: Generate auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,14 +23,4 @@ class User < ApplicationRecord
     role
   end
 
-  # def authenticate(pwd)
-
-  #   command = AuthenticateUser.call(params[:email], params[:password])
- 
-  #   if command.success?
-  #     render json: { auth_token: command.result }
-  #   else
-  #     render json: { error: command.errors }, status: :unauthorized
-  #   end
-  # end
 end
